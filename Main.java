0899c6bf4165d10f8953800d6048433205fe63de
Gabriele <gabriele.manfredi.999@gmail.com>
2019-11-19 23:11:34 +0000

    Test push github

--- Main.java
+++ Main.java
@@ -2,8 +2,11 @@ import it.test.Test;
 
 class Main {
   
+  //Test push github
+
   public static void main(String[] args) {
     Test test = new Test();
     System.out.println(test.doIt());
   }
+
 }
